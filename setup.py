753ef8f8510217b3ed872061e16eb5c60a499876
Kristian Gregorius Hustad <krihus@ifi.uio.no>
2017-07-01 18:52:45 +0200

    Fix typo and email

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(name='odesolver',
       version='1.0',
-      description='A small class hiearachy for solving ordinary differential equations (ODEs)',
+      description='A small class hierarchy for solving ordinary differential equations (ODEs)',
       url='http://github.com/KGHustad/odesolver',
       author='Hans Petter Langtangen',
       #author_email='hpl@simula.no',
       maintainer='Kristian Gregorius Hustad',
-      author_email='krihus@ifi.uio.no',
+      maintainer_email='krihus@ifi.uio.no',
       license='MIT',
       packages=['odesolver'],
       install_requires=[
